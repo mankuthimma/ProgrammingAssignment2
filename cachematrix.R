3f600f59fd22afb90cf2c011a831d3b7b5bedb9d
Shashishekhar S <shashi@inf.in>
2014-05-25 18:24:56 +0530

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -7,16 +7,16 @@
 ## setinverse, getinverse - functions to save the resultant inversed matrix into the cache, obtain the resultant inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
-        x <<- y
-        i <<- NULL
+    i <- NULL                           # Container for the cached inverted matrix
+    set <- function(y) {                
+        x <<- y                         # Store the input matrix in memory, for get() to get
+        i <<- NULL                      # Reset the output container
     }
 
-    get <- function() x
-    setinverse <- function(solve) i <<- solve
-    getinverse <- function() i
-    list(set = set, get = get,
+    get <- function() x                 # Get the original invertible matrix, makeCacheMatrix was called with
+    setinverse <- function(solve) i <<- solve # Set the scope for the container
+    getinverse <- function() i                # Get the inversed matrix from the cache
+    list(set = set, get = get,                # Symbols/Args
          setinverse = setinverse,
          getinverse = getinverse
          )
@@ -31,13 +31,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## data - invertible matrix
 
 cachesolve <- function(x, ...) {
-    i <- x$getinverse()
-    if (!is.null(i)) {
+    i <- x$getinverse()                 # Load the inversed matrix stored in the cache
+    if (!is.null(i)) {                  # Test if an inversed matrix exists
         message("Get cached inverse")
-        return(i)
+        return(i)                       # Send the result and exit
     }
-    data <- x$get()
-    i <- solve(data)
-    x$setinverse(i)
-    i
+    data <- x$get()                     # If the inversed matrix doesn't exist yet, load the invertible matrix into data
+    i <- solve(data)                    # Compute the inverse
+    x$setinverse(i)                     # Store the inversed matrix in the container
+    i                                   # Return the inversed matrix and finis
 }
